8bbd792f306c5543c3307dfc676e26ac959ea29a
Caitriona71 <caitrionaboyle@ymail.com>
2022-10-08 09:58:50 +0000

    Add if& else if statements to represent incorrect guesses on hangman images

--- run.py
+++ run.py
@@ -14,3 +14,45 @@ for x in randomWord:
     print("_", end=" ")
 
 def show_hangman(incorrect):
+    if(incorrect == 0):
+       print("\n+---+")
+       print("     |")
+       print("     |")
+       print("     |")
+       print("    ===")
+    elif(incorrect == 1):
+       print("\n+---+")
+       print("O    |")
+       print("     |")
+       print("     |")
+       print("    ===")                       
+    elif(incorrect == 2):
+       print("\n+---+")
+       print("O    |")
+       print("|    |")
+       print("     |")
+       print("    ===")                           
+    elif(incorrect == 3):
+       print("\n+---+")
+       print(" O   |")
+       print("/|   |")
+       print("     |")
+       print("    ===")
+    elif(incorrect == 4):
+       print("\n+---+")
+       print(" O   |")
+       print("/|\  |")
+       print("     |")
+       print("    ===")
+    elif(incorrect == 5):
+       print("\n+---+")
+       print(" O   |")
+       print("/|\  |")
+       print("/    |")
+       print("    ===")
+    elif(incorrect == 6):
+       print("\n+---+")
+       print(" O   |")
+       print("/|\  |")
+       print("/ \  |")
+       print("    ===")                                   
