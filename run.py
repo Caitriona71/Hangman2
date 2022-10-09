9337777d4b757be14b08c0d096ff2f067cd5c096
Caitriona71 <caitrionaboyle@ymail.com>
2022-10-09 14:37:57 +0000

    Add code to increment the letters the user has guessed correct

--- run.py
+++ run.py
@@ -94,6 +94,7 @@ while(attempts_wrong != 6 and current_letters_correct != length_of_secret_word):
       ### Print random word
       current_guess_index+=1
       current_letters_guessed.append(letterGuessed)
+      current_letters_correct = printWord(current_letters_guessed)
 
 
    
